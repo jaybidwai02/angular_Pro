3a863b55b5500902aa21a52bf41cf2003a46b15d
Jay Bidwai <jay.bidwai@Infoedge.com>
2016-06-09 16:47:45 +0530

    adding todo app

--- js/index.js
+++ js/index.js
@@ -1,28 +1,80 @@
-var app = angular.module('myApp',[]);
+var app = angular.module('myApp',['ngRoute']);
 
-app.controller('myCtr',['$scope',function($scope){
-	$scope.opt = [
-	{
-		id:1,
-		name:'JAY',
-		lname:'Bidwai'
-	},
-	{
-		id:2,
-		name:'Anup',
-		lname:'Bidwai'
-	},
-	{
-		id:3,
-		name:'Pratik',
-		lname:'pad'
-	},
-	{
-		id:3,
-		name:'Prachi',
-		lname:'pad'
+app.config(['$routeProvider',function($routeProvider){
+$routeProvider.when('/',{
+	templateUrl:'routingViews/_index.html',
+	controller:'indexCtr',
+	resolve:{
+		resData : function(apiRequest){
+			return apiRequest.get().then(function(res){
+				return res;
+			});
+		}
 	}
-	];
+}).when('/list/:123',{
+redirectTo:'/',
+controller:'indexCtr'
 
-	$scope.sel = $scope.opt[0];
+}).when('/list',{
+	templateUrl:'routingViews/_pageNameList.html',
+	controller:'listCtr',
+	resolve:{
+		getVal:function($q,$timeout){
+			var myVal = $q.defer();
+			$timeout(function(){
+				myVal.resolve('ddddddddd');
+					//return 'timeout';
+				},1000); 
+
+			return myVal.promise;
+		}
+	}
+
+})
 }]);
+
+app.service('apiRequest',function($http){
+	this.get = function(){
+		return $http.get('js/ddJson.json');
+	}
+})
+
+app.controller('myCtr',['$scope','apiRequest',function($scope,apiRequest){
+	
+	$scope.ddData = {};
+
+		apiRequest.get().then(function(res){
+			$scope.ddData.values = res.data;
+			$scope.sel = $scope.ddData.values[0];
+			//console.log($scope.ddData.values);
+		});
+
+}]);
+
+app.controller('indexCtr',['$scope','apiRequest','resData','$routeParams',function($scope,apiRequest,resData,$routeParams){
+	console.log($routeParams);
+	/*$scope.ddData = {};
+
+	apiRequest.get().then(function(res){
+		$scope.ddData.values = res.data;
+		$scope.sel = $scope.ddData.values[0];
+		//console.log($scope.ddData.values);
+	});*/
+}]);
+
+app.controller('listCtr',['$scope','$timeout','getVal',function($scope,$timeout,getVal){
+	$scope.name = getVal;
+}]);
+
+//custom promise created
+/*var temp = 0;
+var p1 = new Promise(function(resolve,reject){
+	window.setTimeout(function(){
+		temp = 100;
+		resolve(temp);
+	},5000);
+});
+
+p1.then(function(arg){
+	console.log(arg);
+})*/
